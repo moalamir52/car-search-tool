18b28f7fbe6b6e4e8d4dfa6a87531bd1199a2a1e
moalamir52 <Mo.alamir52@gmail.com>
2025-05-20 10:13:17 +0400

    Update App.js

--- App.js
+++ App.js
@@ -142,15 +142,15 @@ export default function CarSearchTool() {
     return isNumericBooking && ejar && invygo && ejar !== invygo;
   }).length;
 
-  const headers = [
+  const headersToShow = [
     "Contract No.",
     "Booking Number",
     "Customer",
-    "Pick-up Branch",
     "EJAR",
     "Model ( Ejar )",
     "INVYGO",
     "Model",
+    "Phone Number",
     "Pick-up Date",
   ];
 
